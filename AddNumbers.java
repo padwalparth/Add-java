948314728ca0285f2668cde16acfbf9d5b45f048
padwalparth <padwalparth73@gmail.com>
2025-08-08 15:14:00 +0530

    heloo parth

---
+++ AddNumbers.java
@@ -0,0 +1,10 @@
+public class AddNumbers {
+    public static void main(String[] args) {
+        int num1 = 5;  // First number
+        int num2 = 10; // Second number
+
+        int sum = num1 + num2; // Adding the two numbers
+
+        System.out.println("Sum: " + sum); // Display the result
+    }
+}
